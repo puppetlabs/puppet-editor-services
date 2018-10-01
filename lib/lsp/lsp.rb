3d02adf62f333622043828e1b3986ba3fb757c19
Glenn Sarti <glennsarti@users.noreply.github.com>
2018-10-01 21:21:04 +0800

    (GH-110) Add custom LSP messages
    The TypeScript to Ruby converter can only generate the LSP specifc methods and
    classes.  Custom created requests and events e.g. PuppetVersionDetails, need
    to be defined separately in the lsp/lsp_custom.rb file.  These onbjects still
    inherit from the LSP base types.

--- lib/lsp/lsp.rb
+++ lib/lsp/lsp.rb
@@ -1,4 +1,4 @@
-%w[lsp_base lsp_enums lsp_protocol lsp_types].each do |lib|
+%w[lsp_base lsp_enums lsp_protocol lsp_types lsp_custom].each do |lib|
   begin
     require "lsp/#{lib}"
   rescue LoadError
