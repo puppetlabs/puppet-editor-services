e33bc3927d89760c86064f979adcd69eee3059d3
Glenn Sarti <glennsarti@users.noreply.github.com>
2018-12-04 16:48:21 +0800

    (GH-88) Add a workspace symbol provider
    The object cache contains all of the type, function and class information so it
    should be easily be able to be queried and then emit these results as part of a
    workspace symbol request (as opposed to document symbol).  This commit adds the
    provider and cache methods to support querying all objects in the cache.

--- lib/languageserver/document_symbol.rb
+++ lib/languageserver/document_symbol.rb
@@ -76,4 +76,62 @@ module LanguageServer
       result
     end
   end
+
+  # /**
+  #  * Represents information about programming constructs like variables, classes,
+  #  * interfaces etc.
+  #  */
+  # interface SymbolInformation {
+  #   /**
+  #    * The name of this symbol.
+  #    */
+  #   name: string;
+
+  #   /**
+  #    * The kind of this symbol.
+  #    */
+  #   kind: number;
+
+  #   /**
+  #    * Indicates if this symbol is deprecated.
+  #    */
+  #   deprecated?: boolean;
+
+  #   /**
+  #    * The location of this symbol. The location's range is used by a tool
+  #    * to reveal the location in the editor. If the symbol is selected in the
+  #    * tool the range's start information is used to position the cursor. So
+  #    * the range usually spans more then the actual symbol's name and does
+  #    * normally include things like visibility modifiers.
+  #    *
+  #    * The range doesn't have to denote a node range in the sense of a abstract
+  #    * syntax tree. It can therefore not be used to re-construct a hierarchy of
+  #    * the symbols.
+  #    */
+  #   location: Location;
+
+  #   /**
+  #    * The name of the symbol containing this symbol. This information is for
+  #    * user interface purposes (e.g. to render a qualifier in the user interface
+  #    * if necessary). It can't be used to re-infer a hierarchy for the document
+  #    * symbols.
+  #    */
+  #   containerName?: string;
+  # }
+  module SymbolInformation
+    def self.create(options)
+      result = {}
+      raise('name is a required field for SymbolInformation') if options['name'].nil?
+      raise('kind is a required field for SymbolInformation') if options['kind'].nil?
+      raise('location is a required field for DocumentSymbol') if options['location'].nil?
+
+      result['name']           = options['name']
+      result['kind']           = options['kind']
+      result['deprecated']     = options['deprecated'] unless options['deprecated'].nil?
+      result['location']       = options['location']
+      result['containerName']  = options['containerName'] unless options['containerName'].nil?
+
+      result
+    end
+  end
 end
