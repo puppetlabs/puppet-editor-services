1a21dde4f9c0bb4abea56494da57b7d7a54a5efe
Glenn Sarti <glennsarti@users.noreply.github.com>
2019-02-22 13:47:52 +0800

    (todo) Add extraction of bolt stuff

---
+++ tools/bolt_extraction/test.rb
@@ -0,0 +1,4 @@
+puts File.expand_path(File.join(File.dirname(__FILE__), '..'))
+
+bolt_module_path = 'C:\\Source\\puppet-editor-services\\tools\\bolt_extraction\\.bundle\\windows\\ruby\\2.4.0\\gems\\bolt-1.10.0\\bolt-modules'
+
