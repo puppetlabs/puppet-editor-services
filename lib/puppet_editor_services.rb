3c060af6ed2c6505c63ab07cf7941208e4445688
Glenn Sarti <glennsarti@users.noreply.github.com>
2019-10-26 21:21:18 +0800

    (GH-193) Rename to puppet_editor_services
    The ruby standard is to use snake-case not kebab-case for namespaces.  This
    commit renames the puppet-editor-services directory to puppet_editor_services.

--- lib/puppet_editor_services.rb
+++ lib/puppet_editor_services.rb
@@ -2,8 +2,8 @@
 
 %w[logging version simple_base simple_tcp_server simple_stdio_server].each do |lib|
   begin
-    require "puppet-editor-services/#{lib}"
+    require "puppet_editor_services/#{lib}"
   rescue LoadError
-    require File.expand_path(File.join(File.dirname(__FILE__), 'puppet-editor-services', 'lib', lib))
+    require File.expand_path(File.join(File.dirname(__FILE__), 'puppet_editor_services', 'lib', lib))
   end
 end
